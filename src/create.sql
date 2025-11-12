ce41e8baf6e78021d744fdaee33dddc36d5ba61a
Fovir <fovir@disroot.org>
2025-11-12 19:36:21 +0800

    feat: Add `Course` table.

--- src/create.sql
+++ src/create.sql
@@ -3,3 +3,14 @@
 DROP DATABASE IF EXISTS StudentCourse;
 CREATE DATABASE StudentCourse;
 USE StudentCourse;
+
+-- Create table `Course`.
+CREATE TABLE Course (
+  CourseCode  VARCHAR(10) PRIMARY KEY NOT NULL,
+  Title       VARCHAR(50) NOT NULL,
+  CreditHour  INT         NOT NULL,
+
+  -- TODO: Check the data type of `Semester`.
+  Semester    INT         NOT NULL
+);
+
