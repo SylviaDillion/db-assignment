84b0b6dc0f09f79de481616be53391a92b8a19c7
Fovir <fovir@disroot.org>
2025-11-13 14:30:58 +0800

    style: Format the code.

--- src/create.sql
+++ src/create.sql
@@ -6,9 +6,9 @@ USE student_course;
 
 -- Create table `Course`.
 CREATE TABLE course (
-  course_code  VARCHAR(10) PRIMARY KEY NOT NULL,
+  course_code VARCHAR(10) PRIMARY KEY NOT NULL,
   title       VARCHAR(50) NOT NULL,
-  credit_hour  INT         NOT NULL,
+  credit_hour INT         NOT NULL,
 
   -- TODO: Check the data type of `Semester`.
   semester    INT         NOT NULL
@@ -19,13 +19,13 @@ CREATE TABLE course (
 --  1. Check the data type of `Duration`.
 --  2. Set `Coordinator` as a foreign key.
 CREATE TABLE programme (
-  programme_code   VARCHAR(10)   PRIMARY KEY NOT NULL,
+  programme_code  VARCHAR(10)   PRIMARY KEY NOT NULL,
   name            VARCHAR(50)   NOT NULL,
   faculty         VARCHAR(50)   NOT NULL,
   duration        INT           NOT NULL DEFAULT 4,
   coordinator     VARCHAR(50)   NULL,
 
-  course_code      VARCHAR(10)   NOT NULL,
+  course_code     VARCHAR(10)   NOT NULL,
   FOREIGN KEY fk_course_course_code(course_code)
     REFERENCES course(course_code),
 
@@ -35,72 +35,72 @@ CREATE TABLE programme (
 
 -- Create table `Student`.
 CREATE TABLE student (
-  matric_number  INT             PRIMARY KEY NOT NULL,
-  firstname     VARCHAR(15)     NOT NULL,
-  lastname      VARCHAR(15)     NOT NULL,
-  birth_date     DATE            NOT NULL,
-  home_street    VARCHAR(30)     NOT NULL,
-  home_city      VARCHAR(15)     NOT NULL,
-  home_postcode  INT             NOT NULL,
-  current_cgpa   DECIMAL(10, 2)  DEFAULT 0,
-  status        VARCHAR(10)     NOT NULL,
-  level         VARCHAR(10)     NOT NULL,
+  matric_number   INT             PRIMARY KEY NOT NULL,
+  firstname       VARCHAR(15)     NOT NULL,
+  lastname        VARCHAR(15)     NOT NULL,
+  birth_date      DATE            NOT NULL,
+  home_street     VARCHAR(30)     NOT NULL,
+  home_city       VARCHAR(15)     NOT NULL,
+  home_postcode   INT             NOT NULL,
+  current_cgpa    DECIMAL(10, 2)  DEFAULT 0,
+  status          VARCHAR(10)     NOT NULL,
+  level           VARCHAR(10)     NOT NULL,
 
-  programme_code VARCHAR(10)     NOT NULL,
+  programme_code  VARCHAR(10)     NOT NULL,
   FOREIGN KEY fk_programme_programme_code(programme_code)
     REFERENCES programme(programme_code)
 );
 
 -- Create table `Staff` to be the superclass of `Advisor` and `Coordinator`.
 CREATE TABLE staff (
-  staff_id         INT           PRIMARY KEY NOT NULL,
-  name            VARCHAR(20)   NOT NULL,
-  contact_number   VARCHAR(11)   NOT NULL,
-  department      VARCHAR(50)   NOT NULL,
-  office_location  VARCHAR(100)  NOT NULL
+  staff_id          INT           PRIMARY KEY NOT NULL,
+  name              VARCHAR(20)   NOT NULL,
+  contact_number    VARCHAR(11)   NOT NULL,
+  department        VARCHAR(50)   NOT NULL,
+  office_location   VARCHAR(100)  NOT NULL
 );
 
 -- Create table `Advisor`.
 CREATE TABLE advisor (
-  staff_id       INT   PRIMARY KEY NOT NULL,
+  staff_id        INT   PRIMARY KEY NOT NULL,
   FOREIGN KEY fk_staff_staff_id(staff_id)
     REFERENCES staff(staff_id),
 
   -- Set to unique to avoid duplicated assignments.
-  matric_number  INT   NOT NULL UNIQUE,
+  matric_number   INT   NOT NULL UNIQUE,
   FOREIGN KEY fk_student_matric_number(matric_number)
     REFERENCES student(matric_number)
 );
 
 -- Create table `Coordinator`.
 CREATE TABLE coordinator (
-  staff_id         INT           PRIMARY KEY NOT NULL,
+  staff_id          INT           PRIMARY KEY NOT NULL,
   FOREIGN KEY fk_staff_staff_id(staff_id)
     REFERENCES staff(staff_id),
 
   -- Avoid duplicated assignments.
-  programme_code   VARCHAR(10)   NOT NULL UNIQUE,
+  programme_code    VARCHAR(10)   NOT NULL UNIQUE,
   FOREIGN KEY fk_programme_programme_code(programme_code)
     REFERENCES programme(programme_code)
 );
 
 -- Create table `Enrollment`.
 CREATE TABLE enrollment (
-  id                INT             PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  academic_session   VARCHAR(10)     NOT NULL,
-  registration_date  DATE            NOT NULL,
-  cgpa              DECIMAL(10, 2)  NOT NULL,
-  final_grade        INT             NULL,
+  id                  INT             PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  academic_session    VARCHAR(10)     NOT NULL,
+  registration_date   DATE            NOT NULL,
+  cgpa                DECIMAL(10, 2)  NOT NULL,
+  final_grade         INT             NULL,
 
-  staff_id           INT             NOT NULL,
+  staff_id            INT             NOT NULL,
   FOREIGN KEY fk_advisor_staff_id(staff_id)
     REFERENCES advisor(staff_id),
 
-  matric_number      INT             NOT NULL,
+  matric_number       INT             NOT NULL,
   FOREIGN KEY fk_student_matric_number(matric_number)
     REFERENCES student(matric_number),
 
-  course_code        VARCHAR(10)     NOT NULL,
+  course_code         VARCHAR(10)     NOT NULL,
   FOREIGN KEY fk_course_course_code(course_code)
     REFERENCES course(course_code)
 );
